d9eb6eb9a825c6d4e19b88601bb3d26096640a5b
Toussaint L Turnier <toussaintturnier98@gmail.com>
2022-04-20 14:37:11 -0400

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () =>{
+return <div className="ui container" style={{marginTop:'10px'}}>text</div>
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/App';
+import SearchBar from './components/SearchBar';
+
+
+
+ReactDOM.render(<SearchBar/>, document.querySelector('#root'));
+
