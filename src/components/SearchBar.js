8f0ebbe7126e499710d9516ebe1a5b621607d56c
Toussaint L Turnier <toussaintturnier98@gmail.com>
2022-04-26 15:35:37 -0400

    Add files via upload

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,30 @@
-import React from "react";
+import React from 'react';
 
-class SearchBar extends React.Component{
-state={term: ''};
+class SearchBar extends React.Component {
+  state = { term: '' };
 
-    // onInputChange(event){
-    //     console.log(event.target.value);
-    // }
+  onFormSubmit = event => {
+    event.preventDefault();
 
-    onInputClick(){
-        console.log('Input was clicked')
-    }
+    this.props.onSubmit(this.state.term);
+  };
 
-    render(){
-        return (<div className="ui segment">
-            <form className="ui form">
-                <div className="field">
-                    <label>Image Search</label>
-                    <input type="text" 
-                    value={this.state.term}
-                    onChange={e => this.setState({term: e.target.value})} 
-                    />
-                </div>
-            </form>
-        </div>
-)
-
-    }
-};
-
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
